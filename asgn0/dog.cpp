ccb9a2c002b41e8141d9cbc3fa7c954ec753d20d
alanc9016 <alcaro@ucsc.edu>
2019-10-01 22:35:57 -0700

    added comments, class makefile

--- asgn0/dog.cpp
+++ asgn0/dog.cpp
@@ -1,44 +1,55 @@
+/*********************************************
+ * Name: Alan Caro
+ * ID: 1650010
+ * Assignment Name:  Assignment 0
+ **********************************************/
+
+#include <cerrno>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <cerrno>
-#include <unistd.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <unistd.h>
 
 void readFiles(int argc, char **argv);
 void readstdin();
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
+    // if arguments are passed, read files
     if(argc > 1)
-        readFiles(argc,argv);
+        readFiles(argc, argv);
+    // read from stdin if no args are passed
     else
         readstdin();
     return 0;
 }
 
-void readFiles(int argc, char **argv){
-    for(int i = 1; i < argc; i++){
+void readFiles(int argc, char **argv)
+{
+    for(int i = 1; i < argc; i++) {
         char buffer[32] = "";
         int fd;
 
         fd = open(argv[i], O_RDONLY);
 
-        if(fd == -1){
-            fprintf(stderr,"open failed: %s\n", strerror(errno));
+        if(fd == -1) {
+            fprintf(stderr, "open failed: %s\n", strerror(errno));
             exit(1);
         }
 
         while(read(fd, buffer, 1))
-            write(1,buffer,1);
+            write(1, buffer, 1);
 
         close(fd);
     }
 }
 
-void readstdin(){
+void readstdin()
+{
     char buffer[32];
 
-    while(read(STDIN_FILENO,buffer, 1) > 0)
-        write(1,buffer,1);
+    while(read(STDIN_FILENO, buffer, 1) > 0)
+        write(1, buffer, 1);
 }
