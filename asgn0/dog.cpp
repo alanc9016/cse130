afd9c98eafad3f0c3678f7db0dca6be1888c14e5
alanc9016 <alcaro@ucsc.edu>
2019-10-05 11:39:46 -0700

    add warn() & warnx()

--- asgn0/dog.cpp
+++ asgn0/dog.cpp
@@ -5,6 +5,7 @@
  **********************************************/
 
 #include <cerrno>
+#include <err.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,39 +18,59 @@ void readstdin();
 
 int main(int argc, char **argv)
 {
-    // if arguments are passed, read files
     if(argc > 1)
-        readFiles(argc, argv);
+        if(strcmp("-", argv[1]) == 0)
+            // read from stdin if "-" is passed
+            readstdin();
+        else
+            // if arguments are passed, read files
+            readFiles(argc, argv);
     // read from stdin if no args are passed
     else
         readstdin();
+
     return 0;
 }
 
 void readFiles(int argc, char **argv)
 {
+    int isInvalidFile = 0;
+    struct stat path_stat;
+
     for(int i = 1; i < argc; i++) {
-        char buffer[32] = "";
+        char buffer[32];
         int fd;
+
+        stat(argv[i], &path_stat);
+
+        if(S_ISDIR(path_stat.st_mode)) {
+            warnx("%s: Is a directory", argv[i]);
+            isInvalidFile = 1;
+            continue;
+        }
 
         fd = open(argv[i], O_RDONLY);
 
         if(fd == -1) {
-            fprintf(stderr, "open failed: %s\n", strerror(errno));
-            exit(1);
+            warn("%s", argv[i]);
+            isInvalidFile = 1;
+            continue;
         }
 
         while(read(fd, buffer, 1))
-            write(1, buffer, 1);
+            write(STDOUT_FILENO, buffer, 1);
 
         close(fd);
     }
+
+    if(isInvalidFile)
+        exit(1);
 }
 
 void readstdin()
 {
     char buffer[32];
 
-    while(read(STDIN_FILENO, buffer, 1) > 0)
-        write(1, buffer, 1);
+    while(read(STDIN_FILENO, buffer, 1))
+        write(STDOUT_FILENO, buffer, 1);
 }
